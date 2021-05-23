0b2bfac852ef0922112c88e7f2293110c7bc072e
Eldos7772002 <82103717+Eldos7772002@users.noreply.github.com>
2021-05-23 22:26:20 +0600

    Add files via upload

---
+++ style.php
@@ -0,0 +1,912 @@
+<?php
+session_start();
+header('Content-Type: text/css');
+$color= $_SESSION['color1'];
+$color2= $_SESSION['color2'];
+$color3= $_SESSION['color3'];
+$color4= $_SESSION['color4'];
+$color5=$_SESSION['color5'];
+$color6=$_SESSION['color6'];
+$color7=$_SESSION['color7'];
+$color8=$_SESSION['color8'];
+$color9=$_SESSION['color9'];
+$color10=$_SESSION['color10'];
+$color11=$_SESSION['color11'];
+$color12=$_SESSION['color12'];
+$color13=$_SESSION['color13'];
+$color15=$_SESSION['color15'];
+$color17=$_SESSION['color17'];
+$color19=$_SESSION['color19'];
+$color21=$_SESSION['color21'];
+$color24=$_SESSION['color24'];
+$color26=$_SESSION['color26'];
+$color29=$_SESSION['color29'];
+$color32=$_SESSION['color32'];
+$color34=$_SESSION['color34'];
+$color36=$_SESSION['color36'];
+$color38=$_SESSION['color38'];
+$color40=$_SESSION['color40'];
+$color43=$_SESSION['color43'];
+$color45=$_SESSION['color45'];
+$color47=$_SESSION['color47'];
+$color49=$_SESSION['color49'];
+$color52=$_SESSION['color52'];
+$color54=$_SESSION['color54'];
+$color57=$_SESSION['color57'];
+?>
+.header {
+background : white;
+height: 104px;
+width: 100%;
+
+}
+.container {
+background: white;
+margin: 0px 189.600px 0px 180.600px; 
+width: 1190px;
+height: 104px;
+
+    background-image: url(https://lh3.googleusercontent.com/proxy/rrudpyeQePwisD6f7cR67EiProICzQkHZqY26cxPN5mgPpAygcET830cAomv7m21FUtB5Il76XkQjKBuZSO4iS136_8QfkR2hPYyT1sbhhqZvjSVKUPo);
+background-color: white;
+}
+.body {
+background: white;
+color: #000000;
+font-family: "Raleway", sans-serif;
+
+
+width: 1500px;
+ 
+    background-attachment: fixed;
+}
+.phone-trust {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    padding: 10px;
+    color: white;
+    font-weight: medium;
+    z-index: 3;
+    border-radius: 20px;
+    text-transform: uppercase;
+    
+    width: 251px;
+    height: 90px;
+     
+    background: #004860;
+    background: linear-gradient #004860;
+
+}
+.media1{
+	width: 231.96px;
+	height:87.2px;
+	 list-style-type: none;
+	
+
+}
+.mr-2{
+	float: left;
+}
+.p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+}
+.media-body{
+	width: 161.96px;
+	height: 87.2px;
+	float: right;
+	color: white
+	
+}
+.mt-0{
+	font-size: 16px;
+	 font-family: "Raleway", sans-serif;
+	 margin: 0px 0px 4px;
+	     font-weight: medium;
+		
+	
+}
+a:visited{   color: white; text-decoration: white 0%; }
+
+.logo{
+float: 	left;	
+margin: 0px auto;
+width: 420px;
+height: 104px;
+background: white;
+
+}
+.logo-text {
+	color: 	black;
+  height: auto;
+  width: 	228px;
+  margin: 	24px 0px 16px 8px;	
+ float: right;
+ right: 42px;
+ font-size: 19px;
+ font-family: "Raleway", sans-serif;
+
+
+}
+.d-flex {
+   display: -ms-flexbox ;
+   display: flex ;
+   width: 	232.31px;
+ 	height: 	41.6px;
+ }
+ .social-media {
+ 	width: 	232.31px;
+ 	height: 	88px;
+ 	position: absolute;	
+ 	left:640px;
+    top:10px;
+    margin: 16px 0px 0px;
+
+
+
+ }
+ .grad {
+    background: rgb(214, 24, 24);
+    background: linear-gradient(
+    90deg
+, rgba(214, 24, 24,1) 0%, rgb(214, 24, 24) 57%);
+    
+ 	width: 	41px;
+ 	height: 	41px;
+ 
+ 	
+ }
+
+ .p-1 {
+    padding: 0.25;
+}
+
+@import url(https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css);
+.social a {
+    text-align: center;
+    width: 35px;
+    height:35px;
+    float: left;
+    background: #fff;
+    border: 1px solid #ccc;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.15), inset 0 0 50px rgba(0,0,0,0.1);
+    border-radius: 24px;
+    margin: 0 10px 10px 0;
+    padding: 6px;
+    color: #000;
+}
+.github a:hover {background: #191919; color: #fff;}
+.youtube a {background: #c4302b; color: #fff;}
+.google-pluse a {background: #DD4B39; color: #fff;}
+.moodle a {background-color: #f98113 ; color:   #f98113}
+.instagram a{background: #3f729b; color: #fff;}
+.facebook a:hover {background: #3b5998; color: #fff;}
+.skype a:hover {background: #00aff0; color: #fff;}
+.vk a:hover {background: #5d84ae; color: #fff;}
+.odnoklassniki a:hover {background: #f93; color: #fff;}
+.pinterest a:hover {background: #c8232c; color: #fff;}
+.linkedin a:hover {background: #0e76a8; color: #fff;}
+.telegram a:hover {background: #249bd7; color: #fff;}
+.tumblr a:hover {background: #34526f; color: #fff;}
+.windows a:hover {background: #125acd; color: #fff;}
+.whatsapp a {background: #50b154; color: #fff;}
+.weibo a:hover {background: #d52b2b; color: #fff;}
+.dropbox a:hover {background: #1087dd; color: #fff;}
+
+
+
+.contact{
+	height: 96px;
+	width:  auto;;
+	float: right;
+	margin-top: 0.5rem;
+	display: block;
+	 font-family: "Raleway", sans-serif;
+	 color: white;
+
+}
+.con-sity{
+	width: 135px;
+	height: 96px;
+	float: left;
+margin: 0px auto;
+display: block;
+ font-family: "Raleway", sans-serif;
+ color: #86868a;
+
+}
+.phone{
+	width: auto;
+	height: 96px;
+float: right;
+margin: 0px auto;
+display: block;
+ font-family: "Raleway", sans-serif;
+color: #86868a;
+}
+
+.float-block{
+  z-index: 3;
+	width: 100%;
+	height: 53.6px;
+	background-color: white;
+    z-index: 1001;
+position:sticky;
+    top: 0px; 
+         box-shadow: 0 5px 10px -5px black;
+}
+.wrapper{
+ 
+}
+
+
+.navbar2{
+	width: 1011px;
+	height: 46px;
+
+	
+	margin: 	0px 238px 0px 238px;
+
+}
+.
+}
+.sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+}
+
+.navbar {
+
+  background-color:white;
+  font-family: Arial;
+
+}
+.collapse{
+	width: 	100%;
+	height: 37px;	;
+}
+
+
+.navbar a {
+  float: left;
+  font-size: 14.5px;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+   
+}
+
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+  font-family: "Raleway", sans-serif;
+  
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 14.5px;
+  border: none;
+  outline: none;
+  color: black;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit; /* Important for vertical align on mobile phones */
+  margin: 0; /* Important for vertical align on mobile phones */
+   
+}
+
+/* Add a red background color to navbar links on hover */
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color:#004860;
+  color: white;
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+  font-size: 	11px;
+}
+
+
+/* Add a grey background color to dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #004860;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.welcome-area {
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(https://cs6.pikabu.ru/post_img/big/2015/06/29/10/1435600206_612568252.jpg);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    height: 100vh;
+    background-attachment: fixed;
+    width: 100%
+    height: 466px;
+}
+.row{
+	float: right;
+}
+.home-feature{
+  height: 700px;
+    padding-bottom: 0px;
+    padding-top: 30px;
+    margin-top: -10px;
+    z-index: 9;
+
+
+}
+.home-feature:img{
+  float: right;
+
+
+}
+.containe2{
+  width: 1140px;
+  height: 337px;
+  margin: 0px 189.600px 0px 80px ;
+}
+.row_2{
+    width: 1140px;
+  height: 337px;
+}
+.features-small-item {
+    cursor: pointer;
+    display: block;
+    background: #FFFFFF;
+    box-shadow: 0 5px 58px 0 rgb(0 0 0 / 10%);
+    -webkit-border-radius: 20px;
+    -moz-border-radius: 20px;
+    border-radius: 0px;
+    padding: 30px;
+    text-align: center;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    transition: all 0.3s ease 0s;
+    position: relative;
+   top:   -300px;
+        font-family: "Raleway", sans-serif;
+         max-width: 900px;
+    height: 800px;
+
+
+}
+.features-small-item2{
+cursor: pointer;
+display: block;
+background: #FFFFFF;
+box-shadow: 0 5px 58px 0 rgb(0 0 0 / 10%);
+-webkit-border-radius: 20px;
+-moz-border-radius: 20px;
+border-radius: 0px;
+padding: 30px;
+text-align: center;
+-webkit-transition: all 0.3s ease 0s;
+-moz-transition: all 0.3s ease 0s;
+-o-transition: all 0.3s ease 0s;
+transition: all 0.3s ease 0s;
+position: relative;
+top:   100px;
+font-family: "Raleway", sans-serif;
+max-width: 900px;
+height: 800px;
+
+
+}
+}
+.col-lg-4{
+      flex: 0 0 33.333333%;
+
+
+    }
+.icon img:hover{
+  background-color: black;
+      transition: all 0.3s ease 0s;
+    width: 90px;
+    height: 90px;
+    line-height: 90px;
+    margin: auto;
+    position: relative;
+    margin-bottom: 30px;
+    background: white;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+}      
+.home-feature2{
+  height: 1000px;
+   
+    padding-top: 30px;
+    margin-top: -10px;
+   
+    z-index: 9;
+}
+.col-lg-40{
+
+}
+ .containe3{
+  width: 960px;
+  height: 337px;
+  margin: 0px 0px 0px 500px ;}
+
+  .features-small-item{
+    margin-top: 130px;s
+  }
+  .welcome-area2{
+     overflow: hidden;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(https://ic.wampi.ru/2021/05/17/IMG_5835.jpg);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    height: 110vh;
+    background-attachment: fixed;
+    width: 100%
+    height: 66px;
+
+
+  }
+  .home-feature3{
+  height: 800px;
+   
+    padding-top: 100px;
+    margin-top: -10px;
+   
+    z-index: 9;
+    text-align: center;
+}
+.home-feature4{
+  height: 800px;
+   
+    padding-top: 100px;
+    margin-top: -10px;
+   
+    z-index: 9;
+    text-align: center;
+}
+.home-feature3 p{
+font-size: 48px;
+font-family: "Raleway", sans-serif;
+font-weight: bold; 
+
+}
+.home-feature4 p{
+font-size: 48px;
+font-family: "Raleway", sans-serif;
+font-weight: bold; 
+
+}
+
+.home-feature3 h6{
+  margin-top: -300px;
+  margin-right: 1100px;
+  font-size: 48px;
+  font-weight: bold; 
+  font-family: "Raleway", sans-serif;
+}
+.home-feature4 h6{
+  margin-top: -300px;
+  margin-right: 1100px;
+  font-size: 48px;
+  font-weight: bold; 
+  font-family: "Raleway", sans-serif;
+}
+.home-feature5{
+  height: 800px;
+   
+    padding-top: 100px;
+    margin-top: -10px;
+   
+    z-index: 9;
+    text-align: center;
+}
+.home-feature5 p{
+font-size: 48px;
+font-family: "Raleway", sans-serif;
+font-weight: bold; 
+
+}
+.home-feature5 h6{
+  margin-top: -300px;
+  margin-right: 1100px;
+  font-size: 48px;
+  font-weight: bold; 
+  font-family: "Raleway", sans-serif;
+}
+.dormentory{
+  width: 700px;
+  height: 505px;
+  margin: 0px auto  ;
+  display: table;
+  font-family: "Raleway", sans-serif;
+  font-weight: bold; 
+  background-color:#FDEAA8;
+  box-shadow:  0 3px 98px 0 rgb(0 0 0 / 40%);
+  margin-top : 150px;
+  outline: 5px solid black;
+}
+
+.cab1{
+  width: 112px;
+  height: 189px;
+  float: left;
+  outline: 5px solid black;
+
+
+}
+.cab21{
+width: 112px;
+height: 189px;
+float: left;
+outline: 5px solid black;
+
+
+}
+.cab21:hover{
+background-color:green;
+background-color:<?php echo $color32;  ?> ;
+}
+.cab2{
+  width: 46px;
+  height: 189px;
+
+  outline: 5px solid black;
+  float: left;
+  margin-left: 5px;
+
+   
+    -webkit-transform: translate(0,0px);
+   }
+.cab22{
+width: 46px;
+height: 189px;
+
+outline: 5px solid black;
+float: left;
+margin-left: 5px;
+
+
+-webkit-transform: translate(0,0px);
+}
+.cab3{
+   width: 74px;
+  height: 189px;
+   outline: 5px solid black;
+
+    transform: translate(0,190px);
+    -webkit-transform: translate(0,0px);
+     margin-left: 5px;
+    float: left;
+}
+.cab23{
+width: 74px;
+height: 189px;
+outline: 5px solid black;
+
+transform: translate(0,190px);
+-webkit-transform: translate(0,0px);
+margin-left: 5px;
+float: left;
+}
+.h5{
+ height: 16px;
+}
+.cab4{
+     width: 131px;
+  height: 189px;
+    outline: 5px solid black;
+  margin-top: -22px;
+    
+   margin-left: 248px;
+}
+.cab24{
+width: 131px;
+height: 189px;
+outline: 5px solid black;
+margin-top: -22px;
+
+margin-left: 248px;
+}
+.cab1:hover {
+background-color:green;
+background-color:<?php echo $color3;  ?> ;
+}
+
+.cab3:hover{
+background-color:green;
+background-color:<?php echo $color5;  ?> ;
+}
+.cab4:hover{
+background-color:green;
+background-color:<?php echo $color8;  ?>
+}
+.cab5:hover{
+background-color:green;
+background-color:<?php echo $color10; ?>;
+}
+.cab6:hover{
+background-color:green;
+background-color:<?php echo $color12; ?>;
+}
+.cab7:hover{
+background-color:green;
+background-color:<?php echo $color15; ?>;
+}
+.cab8:hover{
+background-color:green;
+background-color:<?php echo $color17; ?>;
+}
+.cab9:hover{
+background-color:;
+background-color
+}
+.cab10:hover{
+background-color:green;
+background-color:<?php echo $color19; ?>;
+}
+.cab11:hover{
+background-color:green;
+background-color:<?php echo $color21; ?>;
+}
+.cab12:hover{
+background-color:green;
+background-color:<?php echo $color24; ?>;
+}
+.cab13:hover{
+background-color:green;
+background-color:<?php echo $color26; ?>;
+}
+.cab14:hover{
+background-color:green;
+background-color:<?php echo $color29; ?>;
+}
+.cab23:hover{
+background-color:green;
+background-color:<?php echo $color34; ?>;
+}
+.cab24:hover{
+background-color:green;
+background-color:<?php echo $color36; ?>;
+}
+.cab25:hover{
+background-color:green;
+background-color:<?php echo $color38; ?>;
+}
+.cab26:hover{
+background-color:green;
+background-color:<?php echo $color40; ?>;
+}
+.cab27:hover{
+background-color:green;
+background-color:<?php echo $color43; ?>;
+}
+.cab28:hover{
+background-color:green;
+background-color:<?php echo $color45; ?>;
+}
+.cab210:hover{
+background-color:green;
+background-color:<?php echo $color47; ?>;
+}
+.cab211:hover{
+background-color:green;
+background-color:<?php echo $color49; ?>;
+}
+.cab212:hover{
+background-color:green;
+background-color:<?php echo $color52; ?>;
+}
+.cab213:hover{
+background-color:green;
+background-color:<?php echo $color54; ?>;
+}
+.cab214:hover{
+background-color:green;
+background-color:<?php echo $color57; ?>;
+}
+.dormentory a{
+  color: black    
+}
+.cab5{
+   outline: 5px solid black;
+   width: 131px;
+   height: 110px;
+   margin-left:  384px;
+  -webkit-transform: translate(0,-210px);
+  
+}
+.cab25{
+outline: 5px solid black;
+width: 131px;
+height: 110px;
+margin-left:  384px;
+-webkit-transform: translate(0,-210px);
+
+}
+.cab6{
+  width: 80px;
+   height: 189px;
+      outline: 5px solid black;
+        margin-left:  520px;
+  -webkit-transform: translate(0,-343px);
+}
+.cab26{
+width: 80px;
+height: 189px;
+outline: 5px solid black;
+margin-left:  520px;
+-webkit-transform: translate(0,-343px);
+}
+.cab7{
+  width: 150px;
+   height: 110px;
+      outline: 5px solid black;
+      margin-left:  605px;
+  -webkit-transform: translate(0,-554px);
+}
+.cab27{
+width: 150px;
+height: 110px;
+outline: 5px solid black;
+margin-left:  605px;
+-webkit-transform: translate(0,-554px);
+}
+.cab8{
+   width: 71px;
+   height: 210px;
+      outline: 5px solid black;
+      margin-top: 100px;
+}
+.cab28{
+width: 71px;
+height: 210px;
+outline: 5px solid black;
+margin-top: 100px;
+}
+.cab9{
+  width: 163px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232px;
+         margin-left: 76px;
+}
+.cab29{
+width: 163px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232px;
+margin-left: 76px;
+}
+.cab10{
+  width: 103px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232.5px;
+         margin-left: 244px;
+}
+.cab210{
+width: 103px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232.5px;
+margin-left: 244px;
+}
+.cab11{
+  width: 75px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232px;
+         margin-left: 352px;
+}
+.cab211{
+width: 75px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232px;
+margin-left: 352px;
+}
+.cab12{
+   width: 118px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232px;
+         margin-left: 432px;
+}
+.cab212{
+width: 118px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232px;
+margin-left: 432px;
+}
+.cab13{
+   width: 82px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232.5px;
+         margin-left: 555px;
+}
+.cab213{
+width: 82px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232.5px;
+margin-left: 555px;
+}
+.cab14{
+   width: 111px;
+   height: 210px;
+      outline: 5px solid black;
+         margin-top: -232.5px;
+         margin-left: 642px;
+}
+.cab214{
+width: 111px;
+height: 210px;
+outline: 5px solid black;
+margin-top: -232.5px;
+margin-left: 642px;
+}
+.elfsight-app-14a1b455-7dd0-4916-be93-815509b58a7d{
+  float: right;
+  right: 0px;
+  margin-top: 15px;
+}
+.down{
+  width: 100%;
+  height: 500px;
+  background-color: #f5f5f5;
+}
+.img1{
+top: 40px;
+-webkit-transform: translate(0,-400px);
+margin-left: 1247px;
+}
+.map{
+-webkit-transform: translate(0,50px);
+margin-left: 56px;
+}
+.text{display:none}
+span:hover {word-spacing:0}
+span:hover .text{width220px; background:#f00; display:block;}
+.down p{
+margin-top:-40px;
+margin-left:1250px;
+
+}
